d626e8017d0d37890664ce229f0759ac5f3a363e
Darko Fabijan <darko@renderedtext.com>
2014-07-24 23:53:55 +0200

    Start.

---
+++ lib/chat/models/repo.ex
@@ -0,0 +1,12 @@
+defmodule Chat.Repo do
+  use Ecto.Repo, adapter: Ecto.Adapters.Postgres
+
+  def conf do
+    parse_url "ecto://postgres:postgres@localhost/chat_development"
+  end
+
+  def priv do
+    app_dir(:chat, "priv/repo")
+  end
+
+end
