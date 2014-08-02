ba34da32d1022192f87363c7ada80bbc601958c7
Darko Fabijan <darko@renderedtext.com>
2014-08-02 16:59:24 +0200

    Create room.

--- lib/chat/controllers/rooms.ex
+++ lib/chat/controllers/rooms.ex
@@ -1,5 +1,7 @@
 defmodule Chat.Controllers.Rooms do
   use Phoenix.Controller
+  alias Chat.Repo
+  alias Chat.Room
 
   def index(conn, _params) do
     render conn, "index"
@@ -10,6 +12,10 @@ defmodule Chat.Controllers.Rooms do
   end
 
   def create(conn, %{"room_name" => room_name}) do
+    now = Ecto.DateTime.from_erl(:calendar.universal_time)
+    room = %Room{name: room_name, updated_at: now, created_at: now}
+    Repo.insert(room)
+
     render conn, "create", room_name: room_name
   end
 
