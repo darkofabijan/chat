b38a15453e9f98f674e62ef6339b42ecb8cb1f3c
Darko Fabijan <darko@renderedtext.com>
2014-08-02 14:00:22 +0200

    Fix credentials.

--- lib/chat/models/repo.ex
+++ lib/chat/models/repo.ex
@@ -2,7 +2,7 @@ defmodule Chat.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://postgres:postgres@localhost/chat_development"
+    parse_url "ecto://darko:@localhost/chat_development"
   end
 
   def priv do
