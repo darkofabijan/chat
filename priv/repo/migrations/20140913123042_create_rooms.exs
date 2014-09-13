595e60b6b048f56d3d03ffe9c2ef16d52371490a
Darko Fabijan <darko@renderedtext.com>
2014-09-13 15:21:38 +0200

    Add migration.

---
+++ priv/repo/migrations/20140913123042_create_rooms.exs
@@ -0,0 +1,11 @@
+defmodule Chat.Repo.Migrations.CreateRooms do
+  use Ecto.Migration
+
+  def up do
+    "CREATE TABLE IF NOT EXISTS rooms(id serial primary key, name text)"
+  end
+
+  def down do
+    "DROP TABLE rooms"
+  end
+end
